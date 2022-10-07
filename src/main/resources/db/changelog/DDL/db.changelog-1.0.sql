5ae1d9f6a0d6754f289ebd51089b7b66e0bbe696
Vicitor <victorlukyanchik15@gmail.com>
2022-10-07 18:00:31 +0300

    add name to chats

--- src/main/resources/db/changelog/DDL/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/DDL/db.changelog-1.0.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users_roles;
 DROP TABLE IF EXISTS profiles_groups;
+DROP TABLE IF EXISTS profiles_chats;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS chats;
 DROP TABLE IF EXISTS profiles;
@@ -37,7 +38,7 @@ CREATE TABLE profiles
     lastname      VARCHAR(50)                  NOT NULL,
     email         VARCHAR(100)                 NOT NULL,
     age           INTEGER                      NOT NULL,
-    users_id       BIGINT REFERENCES users (id) NOT NULL,
+    users_id      BIGINT REFERENCES users (id) NOT NULL,
     sex           VARCHAR(15) DEFAULT 'not specified',
     family_status VARCHAR(50) DEFAULT 'not specified',
     town          VARCHAR(50) DEFAULT 'Unknown',
@@ -48,21 +49,22 @@ CREATE TABLE profiles
 
 CREATE TABLE profiles_chats
 (
-    profiles_id BIGINT                NOT NULL REFERENCES profiles (id),
-    chats_id   BIGINT                NOT NULL REFERENCES chats (id)
+    profiles_id BIGINT NOT NULL REFERENCES profiles (id),
+    chats_id    BIGINT NOT NULL REFERENCES chats (id)
 );
 
 CREATE TABLE chats
 (
-    id          BIGSERIAL PRIMARY KEY NOT NULL
+    id   BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50)           NOT NULL
 );
 
 CREATE TABLE messages
 (
-    id         BIGSERIAL PRIMARY KEY NOT NULL,
-    chats_id BIGINT                NOT NULL REFERENCES chats (id),
-    text       VARCHAR(255)          NOT NULL,
-    date_time  TIMESTAMP             NOT NULL
+    id        BIGSERIAL PRIMARY KEY NOT NULL,
+    chats_id  BIGINT                NOT NULL REFERENCES chats (id),
+    text      VARCHAR(255)          NOT NULL,
+    date_time TIMESTAMP             NOT NULL
 );
 
 CREATE TABLE interests
@@ -73,22 +75,22 @@ CREATE TABLE interests
 
 CREATE TABLE groups
 (
-    id          BIGSERIAL PRIMARY KEY NOT NULL,
-    name        VARCHAR(50)           NOT NULL,
+    id           BIGSERIAL PRIMARY KEY NOT NULL,
+    name         VARCHAR(50)           NOT NULL,
     interests_id BIGINT                NOT NULL REFERENCES interests (id)
 );
 
 CREATE TABLE posts
 (
-    id       BIGSERIAL PRIMARY KEY NOT NULL,
+    id        BIGSERIAL PRIMARY KEY NOT NULL,
     groups_id BIGINT                NOT NULL REFERENCES groups (id),
-    title    VARCHAR(80)           NOT NULL,
-    text     VARCHAR(255)          NOT NULL
+    title     VARCHAR(80)           NOT NULL,
+    text      VARCHAR(255)          NOT NULL
 );
 
 CREATE TABLE profiles_groups
 (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    profile_id BIGINT NOT NULL REFERENCES profiles (id),
-    group_id   BIGINT NOT NULL REFERENCES groups (id)
+    id         BIGSERIAL PRIMARY KEY NOT NULL,
+    profile_id BIGINT                NOT NULL REFERENCES profiles (id),
+    group_id   BIGINT                NOT NULL REFERENCES groups (id)
 );
